d5a2c790530ff9b5721e014f1631d527ccdc9c5b
Yaser Hussain <yaserhussain5@gmail.com>
2022-01-22 17:09:08 +0000

    completed validations/associations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,4 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 7 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list" }
 end
